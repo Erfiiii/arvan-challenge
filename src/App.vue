97d129af4309564780a8b735c1eaacf53a08d8d6
erfan hasanzadeh <er.hasanzadeh@gmail.com>
2019-11-15 01:08:15 +0330

    navbar component added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app" class="container-fluid">
+    <header-cm/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import HeaderCm from "./components/Header";
+
+export default {
+  components: {
+    HeaderCm
+  }
+}
+</script>
 
 <style>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-1">
+    <div>
+      <span class="navbar-brand d-inline col-sm-3 col-md-2 mr-0"
+        >Arvan challenge</span
+      >
+      <span class="text col-sm-3 col-md-2 d-inline text-white font-weight-light"
+        >Wellcome Erfan</span
+      >
+    </div>
+    <ul class="navbar-nav px-3 py-2">
+      <li class="nav-item text-nowrap">
+        <button
+          class="nav-link btn text-info py-1 px-2 rounded border border-info"
+        >
+          Logout
+        </button>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "header"
+};
+</script>
